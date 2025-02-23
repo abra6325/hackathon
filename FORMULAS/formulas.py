e7895f3a645fe06ce715ebc0b543fe4d30cc1a67
sh1g <linyi.liu2020@gmail.com>
2025-02-23 16:29:23 +0800

    in distance

--- FORMULAS/formulas.py
+++ FORMULAS/formulas.py
@@ -4,3 +4,6 @@ def energy_use(size, speed, sight):
     e = pow(size, 3) * pow(speed, 2) + sight
     return e
 
+def in_distance(x1, y1, x2, y2, d):
+    return pow(d, 2) == pow(x2 - x1) + pow(y2 - y1)
+
